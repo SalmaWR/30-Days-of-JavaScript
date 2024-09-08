fa8a0e9218b0cf92cd806d8aba2fcb68191eda63
Salma Wagdy <113179672+SalmaWR@users.noreply.github.com>
2024-09-08 13:59:12 +0300

    [2626] [Time Beats: 91.19%] [Memory Beats: 50.42%] - LeetPush

--- 2626-Array-Reduce-Transformation.js
+++ 2626-Array-Reduce-Transformation.js
@@ -6,9 +6,9 @@
  */
 var reduce = function(nums, fn, init) {
     let val = init;
-    for ( let i = 0; i < nums.length; i++) {
-    val = fn(val, nums[i]);
-    }
+    nums.forEach((n)=>{
+        val = fn(val, n);
+    });
 
     return val;
 };
